a19f90d858cbf695c8daac1a35b799b62c78520b
Simón Montoya Bedoya <simonmontoyabedoya@gmail.com>
2021-07-18 16:14:33 -0500

    Adicionar funcionalidad y archivo readme

--- js/index.js
+++ js/index.js
@@ -7,17 +7,35 @@ function guardarBusqueda(valor) {
     const historial = localStorage.getItem("historial");
     const historialArray = JSON.parse(historial);
     historialArray.push(valor);
-    localStorage.setItem("historial", historialArray);
+    console.log(historialArray);
+    localStorage.setItem("historial", JSON.stringify(historialArray));
+    return historialArray;
 }
 
+function crearHistorial() {
+    if (localStorage.getItem("historial") === null) {
+        localStorage.setItem("historial", "[]");
+    }
+}
 
+function renderizarHistorial(historialVista, busquedasRealizadas) {
+    historialVista.innerHTML = "";
+    busquedasRealizadas.forEach((item) => {
+        const template = `<p>${item}</p>`;
+        historialVista.innerHTML += template;
+
+    });
+}
 window.onload = () => {
     const form = document.forms.searchForm;
-
+    const historial = document.querySelector("#busquedas-realizadas")
+    crearHistorial();
     form.addEventListener('submit', (event) => {
         event.preventDefault();
         const valor = form.busqueda.value;
         const url = `https://www.google.com/search?q=${valor}`;
+        busquedasRealizadas = guardarBusqueda(valor);
+        renderizarHistorial(historial, busquedasRealizadas);
         redireccionarBusqueda(url);
 
     });
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Buscador Sencillo. ✏️
+
+Se trata de un buscador sencillo que realiza búsquedas en google y va almacenando el historial de lo buscado 👀 👀 👀
+
+## Página
+El ejercicio realizado se puede encontrar en la siguiente página: 
+[Links](https://simontoyabe.github.io/BuscadorSencillo/)
